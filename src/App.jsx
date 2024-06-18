b09e59ee20a944ce4093f76251a6f98d81f806c5
J.K. Nithin Kumar <89259785+jknithin36@users.noreply.github.com>
2024-06-17 20:22:10 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
-import Model from "./components/Model";
+
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import HowItWorks from "./components/HowItWoeks";
@@ -12,7 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
-      <Model />
+    
       <Features />
       <HowItWorks />
       <Footer />
